50229893ab0d1026c5eecb5021a7a4014c0b1b0a
SeongUk18 <adulty22@gmail.com>
2024-06-12 13:51:06 +0900

    Feat: 스키마 요청 구현

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,8 +2,24 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, examples=["세탁소에 맡긴 것을 찾으러 가기"])
+
+
+class TaskCreate(TaskBase):  # TaskBase를 변경하지 않으면 동작 확인 시 title이 표시되지 않음
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
     id : int
-    title: Optional[str] = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
+
+    class Config:
+        from_attributes = True
+
+
+class Task(TaskBase):
+    id : int
     done : bool = Field(False, description="완료 플래그")
 
+    class Config:
+        from_attributes = True
