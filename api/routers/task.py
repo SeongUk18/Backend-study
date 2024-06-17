4903e68a661ebacd74b6913cd9e6b6251f7b9b1c
SeongUk18 <adulty22@gmail.com>
2024-06-17 13:33:53 +0900

    Feat: Delete 구현

--- api/routers/task.py
+++ api/routers/task.py
@@ -10,29 +10,31 @@ from api.db import get_db
 router = APIRouter()
 
 
-@router.get("/tasks", response_model=list[task_schema.Task])
-async def list_tasks(db:Session=Depends(get_db)):
+@router.get("/tasks", response_model = list[task_schema.Task])
+async def list_tasks(db: Session = Depends(get_db)):
     return task_crud.get_tasks_with_done(db)
 
 
-@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate, db:Session=Depends(get_db)):
+@router.post("/tasks", response_model = task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate, db: Session = Depends(get_db)):
     return task_crud.create_task(db, task_body)
 
 
-@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+@router.put("/tasks/{task_id}", response_model = task_schema.TaskCreateResponse)
 async def update_task(
     task_id: int, task_body: task_schema.TaskCreate, db: Session=Depends(get_db)
     ):
     task = task_crud.get_task(db, task_id = task_id)
     if task is None:
-        raise HTTPException(status_code=404, detail="Task not found")
+        raise HTTPException(status_code = 404, detail = "Task not found")
     return task_crud.update_task(db, task_body, original=task)
     
 
 @router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass
-
+async def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = task_crud.get_task(db, task_id = task_id)
+    if task is None:
+        raise HTTPException(status_code = 404, detail = "Task not found")
+    return task_crud.delete_task(db, original = task)
 
 
