f76b2bb2c29e734176d41089de297a2a7e2f243f
SeongUk18 <adulty22@gmail.com>
2024-06-18 17:19:38 +0900

    Feat: task 기능 비동기화

--- api/routers/task.py
+++ api/routers/task.py
@@ -12,8 +12,8 @@ router = APIRouter()
 
 
 @router.get("/tasks", response_model = list[task_schema.Task])
-async def list_tasks(db: Session = Depends(get_db)):
-    return task_crud.get_tasks_with_done(db)
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 
 @router.post("/tasks", response_model = task_schema.TaskCreateResponse)
@@ -23,19 +23,19 @@ async def create_task(task_body: task_schema.TaskCreate, db: AsyncSession = Depe
 
 @router.put("/tasks/{task_id}", response_model = task_schema.TaskCreateResponse)
 async def update_task(
-    task_id: int, task_body: task_schema.TaskCreate, db: Session=Depends(get_db)
+    task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
     ):
-    task = task_crud.get_task(db, task_id = task_id)
+    task = await task_crud.get_task(db, task_id = task_id)
     if task is None:
         raise HTTPException(status_code = 404, detail = "Task not found")
-    return task_crud.update_task(db, task_body, original=task)
+    return await task_crud.update_task(db, task_body, original=task)
     
 
 @router.delete("/tasks/{task_id}")
-async def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = task_crud.get_task(db, task_id = task_id)
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_crud.get_task(db, task_id = task_id)
     if task is None:
         raise HTTPException(status_code = 404, detail = "Task not found")
-    return task_crud.delete_task(db, original = task)
+    return await task_crud.delete_task(db, original = task)
 
 
