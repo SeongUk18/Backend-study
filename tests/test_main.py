8faaea0acbb6cf41bc0369838c4bf9b50cbefb87
SeongUk18 <adulty22@gmail.com>
2024-06-22 17:05:41 +0900

    Feat: test 코드 작성

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,7 @@ import pytest_asyncio
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+import starlette.status
 
 from api.db import get_db, Base
 from api.main import app
@@ -35,3 +36,41 @@ async def async_client() -> AsyncClient:
     # 테스트 용으로 비동기 HTTP 클라이언트 반환
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
+
+
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+    response = await async_client.post("/tasks", json={"title": "테스트 작업"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert "id" in response_obj
+    assert response_obj["title"] == "테스트 작업"
+
+
+@pytest.mark.asyncio
+async def test_done_flag(async_client):
+    # Create a new task
+    response = await async_client.post("/tasks", json={"title": "테스트 작업2"})
+    assert response.status_code == starlette.status.HTTP_200_OK
+    response_obj = response.json()
+    assert "id" in response_obj
+    assert response_obj["title"] == "테스트 작업2"
+
+    task_id = response_obj["id"]
+
+    # 완료 플래그 설정
+    response = await async_client.put(f"/tasks/{task_id}/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # 이미 완료 플래그가 설정되어 있으므로 400을 반환
+    response = await async_client.put(f"/tasks/{task_id}/done")
+    assert response.status_code == starlette.status.HTTP_400_BAD_REQUEST
+
+    # 완료 플래그 해제
+    response = await async_client.delete(f"/tasks/{task_id}/done")
+    assert response.status_code == starlette.status.HTTP_200_OK
+
+    # 이미 완료 플래그가 해제되었으므로 404 반환
+    response = await async_client.delete(f"/tasks/{task_id}/done")
+    assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
