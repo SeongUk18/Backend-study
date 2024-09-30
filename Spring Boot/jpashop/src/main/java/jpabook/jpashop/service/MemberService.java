915f318e21c7161ccda513cf47fd6cfa3ca7e54c
SeongUk18 <adulty22@gmail.com>
2024-09-30 14:13:01 +0900

    Feat: member service 구현

---
+++ Spring Boot/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 모든 데이터 변경들은 트랜젝션안에서 실행 돼야함. 조회만 할때는 readOnly를 사용하면 효율이 더 좋아짐
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    // 회원 가입
+    @Transactional // 여기가 더 우선권임.
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
