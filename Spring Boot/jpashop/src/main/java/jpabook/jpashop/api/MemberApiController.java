548f9e0ebfa966f113d1a8e1524cc1415386e36c
SeongUk18 <adulty22@gmail.com>
2024-11-06 13:12:43 +0900

    Feat: 개선된 사용자명 등록 기능 구현(엔티티 영향 줄이기)

--- Spring Boot/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ Spring Boot/jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -16,9 +16,25 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping("/api/v1/members")
+    // api 생성시 엔티티를 직접 받는것은 안좋음
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
     }
 
     @Data
